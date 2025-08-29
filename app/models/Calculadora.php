61a63276c8d02fa04cd4f1f1bf16c6bb2f484d69
Randy <randy26smc@gmail.com>
2025-08-29 03:27:16 -0600

    agregando funcionalidad de division

--- app/models/Calculadora.php
+++ app/models/Calculadora.php
@@ -1,20 +1,30 @@
 <?php
 
-class Calculadora{
+class Calculadora
+{
 
-    public function sumar($num1, $num2){
+    public function sumar($num1, $num2)
+    {
         return $num1 + $num2;
     }
 
-    public function restar($num1, $num2){
+    public function restar($num1, $num2)
+    {
         return $num1 - $num2;
     }
 
-    public function multiplicar($num1, $num2){
+    public function multiplicar($num1, $num2)
+    {
         return $num1 * $num2;
     }
 
+    public function dividir($num1, $num2)
+    {
+        if ($num2 == 0) {
+            throw new InvalidArgumentException("No se puede dividir entre cero");
+        }
+        return $num1 / $num2;
+    }
 }
 
 ?>
-
